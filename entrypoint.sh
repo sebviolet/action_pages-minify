2303ca6dce5948c5fb5dfca9587f38356fbb5037
sebos <15905816+sebviolet@users.noreply.github.com>
2024-11-07 13:31:32 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,7 @@ npm i minify -g
 
 find . -type f \( -iname \*.html -o -iname \*.js -o -iname \*.css \) | while read fname
     do
-    minify ${fname} | sponge ${fname}
+    #minify ${fname} | sponge ${fname}
+    minify ${fname} > ${fname}.mini
+    mv ${fname}.mini ${fname}
     done
