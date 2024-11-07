f761ec64288f076bfe29a511d89ff59d5df8b326
sebos <15905816+sebviolet@users.noreply.github.com>
2024-11-07 13:45:18 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/sh -l
 
 apt-get update
-apt-get -y install moreutils
 npm i minify -g
 
 find . -type f \( -iname \*.html -o -iname \*.js -o -iname \*.css \) | while read fname
-    do
-    #minify ${fname} | sponge ${fname}
+do
     minify ${fname} > ${fname}.mini
     mv ${fname}.mini ${fname}
-    done
+done
